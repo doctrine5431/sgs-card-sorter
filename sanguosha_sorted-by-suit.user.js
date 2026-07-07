26037559d8c49482ee80a7348b2c4e7e45b7b07c
doctrine5431 <106268405+doctrine5431@users.noreply.github.com>
2026-07-07 15:23:46 +0800

    Rename project for sanguosha sorted by suit

--- sanguosha_sorted-by-suit.user.js
+++ sanguosha_sorted-by-suit.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         三国杀手牌花色排序
-// @namespace    sgs-card-sorter
+// @namespace    sanguosha_sorted-by-suit
 // @version      2.1
 // @description  可拖拽悬浮窗，点击按花色排序手牌
 // @author       FAWEI
